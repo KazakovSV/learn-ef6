e2ec69f314242a771e6cd9c026b3d2edbc50fdd3
Stanislav Kazakov <kazakovsv@tut.by>
2020-06-17 17:23:48 +0300

    Add P01_DatabaseFirst project

---
+++ LearnEF6/P01_DatabaseFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new TestDbEntities();
+            var entities = context.PersonCards.ToList();
+
+            foreach (var entity in entities)
+            {
+                Console.WriteLine($"{entity.Id}: {entity.FirstName} {entity.SecondName}");
+            }
+        }
+    }
+}
